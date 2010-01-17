4d7899a608638ffc59526c3d3dda34bdd3259ea3
Tatham Oddie <tatham@oddie.com.au>
2010-01-17 23:19:13 +1100

    Revert "New Sinatra app codE"
    This reverts commit bf82d9883c01a57b89e5678cc596c12455d99de2.

--- config.ru
+++
@@ -1,3 +0,0 @@
-require "longitude"
-
-run Sinatra::Application
--- longitude.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-
-get '/' do
-  'This text was rendered by Sinatra, written in Ruby, hosted on Heroku with a custom domain name and deployed via git!'
-end
