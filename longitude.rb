72a9c63fc90c66b73e94c0b9a31551c89187d77d
Tatham Oddie <tatham@oddie.com.au>
2010-01-18 20:07:56 +1100

    Fiddling with Twitter code

--- longitude.rb
+++ longitude.rb
@@ -6,28 +6,42 @@ require 'json'
 
 load "#{File.dirname(__FILE__)}/lib/geo.rb"
 
-get '/' do
-  haml :default
-end
-
 get '/styles.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
   sass :styles
 end
 
-get '/location-feed/:id' do |id|
+get %r{/([\w]*)$} do |id|
+  @feed_path = "/#{id}/feed.kml"
+  haml :default
+end
+
+get %r{/([\w]*)/feed.([\w]+)$} do |id,format|
   header 'Content-Type' => 'application/vnd.google-earth.kml+xml'
+
   timeline = Twitter.timeline(id, { :count => 200 })
-  @coordinates = []
-  for i in (0..timeline.length-1)
-    tweet = timeline[i]
-    next if tweet.geo == nil
-    next if tweet.geo.type != "Point"
-    if @coordinates.length > 0 then
-      # ignore it if it's within 500m of the last point
-      next if Geo.calculate_displacement(@coordinates[@coordinates.length-1], tweet.geo.coordinates) < 0.5
-    end
-    @coordinates << tweet.geo.coordinates
+
+  if timeline.length == 0 then
+    raise Sinatra::NotFound, "No tweets found in timeline."
   end
-  haml :kml
+
+  timeline.to_json
+
+#  @coordinates = []
+#  for i in (0..timeline.length-1)
+#    tweet = timeline[i]
+#    next if tweet.geo == nil
+#    next if tweet.geo.type != "Point"
+#    if @coordinates.length > 0 then
+#      # ignore it if it's within 500m of the last point
+#      next if Geo.calculate_displacement(@coordinates[@coordinates.length-1], tweet.geo.coordinates) < 0.5
+#    end
+#    @coordinates << tweet.geo.coordinates
+#  end
+#
+#  if (format == 'kml') then
+#    haml :kml
+#  else
+#    raise Sinatra::NotFound, "Format not recognized."
+#  end
 end
