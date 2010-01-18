cebf60b340f9a0e2808c32544fc6a3921bd59555
Tatham Oddie <tatham@oddie.com.au>
2010-01-18 20:33:28 +1100

    Getting serious now ...

--- longitude.rb
+++ longitude.rb
@@ -19,29 +19,34 @@ end
 get %r{/([\w]*)/feed.([\w]+)$} do |id,format|
   content_type 'application/vnd.google-earth.kml+xml', :charset => 'utf-8'
 
-  timeline = Twitter.timeline(id, { :count => 200 })
+#  timeline = Twitter.timeline(id, { :count => 200 })
+#
+#  if timeline.length == 0 then
+#    raise Sinatra::NotFound, "No tweets found in timeline."
+#  end
 
-  if timeline.length == 0 then
-    raise Sinatra::NotFound, "No tweets found in timeline."
+  test = []
+  for i in (0..5)
+    test << i
   end
+  test.to_json
 
-  coordinates = []
-  for i in (0..timeline.length-1)
-    tweet = timeline[i]
-    next if tweet.geo == nil
-    next if tweet.geo.type != "Point"
-    if coordinates.length > 0 then
-      # ignore it if it's within 500m of the last point
-      next if Geo.calculate_displacement(coordinates[coordinates.length-1], tweet.geo.coordinates) < 0.5
-    end
-    coordinates << tweet.geo.coordinates
-  end
-
-  if (format == 'kml') then
-    haml :kml, :locals => { :coordinates => coordinates }
-  elsif (format == 'json') then
-    coordinates.to_json
-  else
-    raise Sinatra::NotFound, "Format not recognized."
-  end
+#  for i in (0..timeline.length-1)
+#    tweet = timeline[i]
+#    next if tweet.geo == nil
+#    next if tweet.geo.type != "Point"
+#    if coordinates.length > 0 then
+#      # ignore it if it's within 500m of the last point
+#      next if Geo.calculate_displacement(coordinates[coordinates.length-1], tweet.geo.coordinates) < 0.5
+#    end
+#    coordinates << tweet.geo.coordinates
+#  end
+#
+#  if (format == 'kml') then
+#    haml :kml, :locals => { :coordinates => coordinates }
+#  elsif (format == 'json') then
+#    coordinates.to_json
+#  else
+#    raise Sinatra::NotFound, "Format not recognized."
+#  end
 end
